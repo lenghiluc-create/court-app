640d5d76ac5638cc54fb0fe2d6a6e474687c77fb
Toa An Admin <admin@toaan.vn>
2026-04-22 18:20:32 +0700

    Khởi tạo dự án tòa án

---
+++ app/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+ apiKey: "AIzaSyB2nBRjYxAu9cjFIGpf1NoFsObS7ZhIV6g",
+  authDomain: "tandkv9-ct.firebaseapp.com",
+  projectId: "tandkv9-ct",
+  storageBucket: "tandkv9-ct.firebasestorage.app",
+  messagingSenderId: "10740620306",
+  appId: "1:10740620306:web:3ba92c258cb8bf24d24ccb",
+  measurementId: "G-QSXLX7K0YJ"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
